cb4a8774d9a9dd91ea80c702d71a8b0354b120c5
Jon Stombaugh <jon@stonetapestudios.com>
2025-07-28 02:48:19 -0400

    sign in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { SignIn } from '@auth/sveltekit/components';
+	import { page } from '$app/state';
+</script>
+
+<h1 class="py-4 text-center text-2xl font-bold">
+	Welcome to Stone Tape Studios SvelteKit Template
+</h1>
+<div class="flex justify-center">
+	<SignIn
+		options={{
+			redirectTo: page.data.redirectTo
+				? `/${decodeURIComponent(page.data.redirectTo).slice(1)}`
+				: `/dashboard`
+		}}
+		provider="keycloak"
+		signInPage="signin"
+	/>
+</div>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="py-4 text-center text-2xl font-bold">Dashboard</h1>
