2b1f9c8de99037186b92ea62eca40539b09652f0
Jon Stombaugh <jon@stonetapestudios.com>
2025-07-28 23:47:04 -0400

    tanstack devtools

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+
 	import '../app.css';
 	import type { LayoutData } from './$types';
 	import type { Snippet } from 'svelte';
@@ -9,4 +11,5 @@
 
 <QueryClientProvider client={data.queryClient}>
 	{@render children()}
+	<SvelteQueryDevtools />
 </QueryClientProvider>
